c4428b03e089a97430d9408b8c922c08f005150e
Carlos Eduardo X. Bezerra <47821159+Carlos-Xavier@users.noreply.github.com>
2022-02-01 22:42:31 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <cmath>
+#include <stdio.h>
 
+float ballRotateX, ballRotateY, speedX, speedY = 0.0;
+float auxSpeedX, auxSpeedY = 0.0;
 
 void field()
 {
@@ -40,6 +43,14 @@ void setView()
         );
 }
 
+void drawString(float x, float y, float z, char *string) {
+        glTranslatef(-3, 0, .3);
+        glScalef(1/152, 1/152, 1/152);
+        for( char* p = string; *p; p++)
+        {
+            glutStrokeCharacter(GLUT_STROKE_ROMAN, *p);
+        }
+}
 
 void drawLines()
 {
@@ -159,29 +170,40 @@ void curves()
 void beam()
 {
     glColor3f(1, 1, 0.9);
-    	glBegin(GL_LINES);
+    glBegin(GL_LINES);
 
-	// Esquerda
-    glVertex3f(-0.9, -2.1, 0.3);
-    glVertex3f(-0.1, -2.1, 0.3);
+        // Esquerda
+        glVertex3f(-0.9, -2.1, 0.3);
+        glVertex3f(-0.1, -2.1, 0.3);
 
-    glVertex3f(-0.9, -2.1, 0.3);
-    glVertex3f(-0.3, -2.1, 0.1);
+        glVertex3f(-0.9, -2.1, 0.3);
+        glVertex3f(-0.3, -2.1, 0.1);
 
-    glVertex3f(-0.1, -2.1, 0.3);
-    glVertex3f(.3, -2.1, 0.1);
+        glVertex3f(-0.1, -2.1, 0.3);
+        glVertex3f(.3, -2.1, 0.1);
 
-    // Direita
-    glVertex3f(-.9, 2.1, 0.3);
-    glVertex3f(-.1, 2.1, 0.3);
+        // Direita
+        glVertex3f(-.9, 2.1, 0.3);
+        glVertex3f(-.1, 2.1, 0.3);
 
-    glVertex3f(-0.9, 2.1, 0.3);
-    glVertex3f(-0.3, 2.1, 0.1);
+        glVertex3f(-0.9, 2.1, 0.3);
+        glVertex3f(-0.3, 2.1, 0.1);
 
-    glVertex3f(-0.1, 2.1, 0.3);
-    glVertex3f(.3, 2.1, 0.1);
+        glVertex3f(-0.1, 2.1, 0.3);
+        glVertex3f(.3, 2.1, 0.1);
 
 	glEnd();
+}
+
+void ball()
+{
+
+    glRotatef(1.5, 1, 0, 0);
+    glTranslatef(speedX,speedY, .3);
+    glColor3f(1.0,0.9,0.0);
+    glScalef(0.04,0.04,0.04);
+    glutSolidSphere(1,50,50);
+
 }
 
 void display()
@@ -195,8 +217,76 @@ void display()
     drawLines();
     beam();
     curves();
+    ball();
+    drawString(-2, 0, .5, "OPA");
 
     glutSwapBuffers();
+}
+
+void handleKeypress(unsigned char key, int x, int y) {
+    switch (key) {
+        case 97:
+            auxSpeedY = -0.025;
+            break;
+        case 100:
+            auxSpeedY = 0.025;
+            break;
+        case 115:
+            auxSpeedX = 0.025;
+            break;
+        case 119:
+            auxSpeedX = -0.025;
+            break;
+        case 27:
+            exit(0);
+    }
+}
+
+void KeyboardUpHandler(unsigned char key, int x, int y) {
+    switch (key) {
+        case 97:
+            auxSpeedY = 0;
+            break;
+        case 100:
+            auxSpeedY = 0;
+            break;
+        case 115:
+            auxSpeedX = 0;
+            break;
+        case 119:
+            auxSpeedX = 0;
+            break;
+        case 27:
+            exit(0);
+    }
+}
+
+void update(int value)
+{
+    speedX += auxSpeedX;
+    if ((ballRotateX > -0.3 && ballRotateX < 0.3) && auxSpeedX != 0)
+        ballRotateX += speedX / 15;
+
+    speedY += auxSpeedY;
+    if ((ballRotateY > -0.3 && ballRotateY < 0.3) && auxSpeedY != 0)
+        ballRotateY += speedY / 15;
+
+
+
+    if ((speedX > -0.3 && speedX < 0.3) && speedY <= -2)
+    {
+        printf("GOOOOOOOOOL DO TIME VERMELHO", speedY);
+        speedX = speedY = 0;
+    }
+
+    if ((speedX > -0.3 && speedX < 0.3) && speedY >= 2)
+    {
+        printf("GOOOOOOOOOL DO TIME AZUL", speedY);
+        speedX = speedY = 0;
+    }
+
+    glutPostRedisplay();
+    glutTimerFunc(20, update, 0);
 }
 
 int main( int argc, char **argv )
@@ -205,7 +295,10 @@ int main( int argc, char **argv )
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
     glutInitWindowSize(1200, 800);
     glutCreateWindow("FIFA");
+    glutKeyboardFunc(handleKeypress);
+    glutKeyboardUpFunc(KeyboardUpHandler);
     glutDisplayFunc(display);
+    glutTimerFunc(25, update, 0);
     glutMainLoop();
     return 0;
 }
